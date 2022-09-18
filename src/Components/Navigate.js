22eb1d36808ea55a9b85eabdc9ee173e229d764a
PanDonut <81489726+PanDonut@users.noreply.github.com>
2022-09-18 18:15:39 +0200

    dev

--- src/Components/Navigate.js
+++
@@ -1,8 +0,0 @@
-export function NavigateTo(navigate, to) {
-    var Page = document.getElementsByClassName("Page")[0];
-    Page.style.opacity = 0;
-    setTimeout(() => {
-        navigate(to);
-        Page.style.opacity = 1;
-    }, 500)
-}
